4b5e89081eed340470546620c2f6f5e617f7c15c
Serhii T <tocarchuk@gmail.com>
2021-12-13 22:18:30 +0200

    Rollback

--- db/migrate/20211213164147_create_enrollments.rb
+++
@@ -1,8 +0,0 @@
-class CreateEnrollments < ActiveRecord::Migration[6.1]
-  def change
-    create_table :enrollments do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_164147) do
-
-  create_table "enrollments", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_12_13_114018) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
